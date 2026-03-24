33265a2aa2fd47951f1a2bc9d5b5d65b4a767dd4
Manas Bhasin <107252808+Manasbh@users.noreply.github.com>
2026-03-24 18:50:52 +0530

    update

--- bin/setup-venv.sh
+++ bin/setup-venv.sh
@@ -6,7 +6,7 @@ cd "$ROOT"
 
 python3 -m venv .venv
 source .venv/bin/activate
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip setuptools wheel
 python -m pip install -e .
 
 echo "KDX virtualenv ready: $ROOT/.venv"
